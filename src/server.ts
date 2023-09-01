0dacb1c3a2d91a91124c3bc51b52cc98dd4124b4
osmansufy <osmansufy20@gmail.com>
2023-09-01 16:54:04 +0600

    Add husky eslint prettier in project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express, { Application } from "express";
+
+import httpStatus from "http-status";
+
+import cookieParser from "cookie-parser";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(httpStatus.OK).send("Hello World!");
+});
